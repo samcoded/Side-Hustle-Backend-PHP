8213accce9814b5d71d05e231e2df1cde0331206
Samuel Anozie <72805827+samcoded@users.noreply.github.com>
2020-10-28 21:14:03 +0100

    Update connect1.php

--- week3/connect1.php
+++ week3/connect1.php
@@ -10,7 +10,7 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 
 // Check connection 
 if(mysqli_connect_errno()){
-echo "Error in connection!".mysqli_connect_error();
+echo "Error in connection: ".mysqli_connect_error();
 exit();
 }
 echo "Connected successfully";
